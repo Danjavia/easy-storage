4028cdb379a69b0d5dc9cf61acc2ff8289421500
Danny Viasus(Danjavia) <danny.viasus@comparamejor.com>
2016-06-09 11:56:51 -0500

    npm init

--- src/index.js
+++ src/index.js
@@ -1,63 +1,73 @@
 import store from 'store2'
 import _ from 'underscore'
 
-class Uj4Helper {
+class SHelper {
 
-	update ( ...args ) {
+    update ( ...args ) {
 
-		if ( args.length > 2 ) {
+        if ( args.length > 2 ) {
 
-			try {
+            try {
 
-				if ( store.has( args[ 0 ] ) ) {
+                if ( store.has( args[ 0 ] ) ) {
 
-					let data = JSON.parse( store.get( args[ 0 ] ) )
+                    let data = JSON.parse( store.get( args[ 0 ] ) )
 
-					data[ args[ 1 ] ] = args[ 2 ]
+                    data[ args[ 1 ] ] = args[ 2 ]
 
-					store.set( args[ 0 ], JSON.stringify( data ) )
+                    store.set( args[ 0 ], JSON.stringify( data ) )
 
-					return true
-				}
+                    return true
+                }
 
-				else
-					store.set( args[ 0 ], JSON.stringify( args[ 2 ] ) )
+                else {
+                    let data = {}
+
+                    data[args[ 1 ]] = args[ 2 ]
+
+                    store.set( args[ 0 ], JSON.stringify( data ) )
+                }
 
 
-			} catch( e ) {
+            } catch( e ) {
 
-				// statements
-				console.log( `You has the following error:  ${ e }` )
-			}
-		}
+                // statements
+                console.log( `You has the following error:  ${ e }` )
+            }
+        }
 
-		else {
+        else {
 
-			if ( store.has( args[ 0 ] ) ) {
+            if ( store.has( args[ 0 ] ) ) {
 
-				let data = JSON.parse( store.get( args[ 0 ] ) )
+                let data = JSON.parse( store.get( args[ 0 ] ) )
 
-				// let newState = Object.assign( data, args[ 1 ] )
-				let newState = _.extend( data, args[ 1 ] )
+                // let newState = Object.assign( data, args[ 1 ] )
+                let newState = _.extend( data, args[ 1 ] )
 
-				store.set( args[ 0 ], JSON.stringify( newState ) )
-			}
+                store.set( args[ 0 ], JSON.stringify( newState ) )
+            }
 
-		}
-	}
+            else {
+                let data = args[ 1 ]
 
-	log ( entity ) {
-		let logData = JSON.parse( store.get( entity ) )
-		debugger
-	}
+                store.set( args[ 0 ], JSON.stringify( data ) )
+            }
+        }
+    }
 
-	has ( entity ) {
-		return store.has( entity ) ? true : false
-	}
+    log ( entity ) {
+        let logData = JSON.parse( store.get( entity ) )
+        debugger
+    }
 
-	get ( entity ) {
-		return JSON.parse( store.get( entity ) )
-	}
+    has ( entity ) {
+        return store.has( entity ) ? true : false
+    }
+
+    get ( entity ) {
+        return JSON.parse( store.get( entity ) )
+    }
 }
 
-export default new Uj4Helper()
+export default new SHelper()
